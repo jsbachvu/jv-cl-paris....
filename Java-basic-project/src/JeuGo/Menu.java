0802d48e4d641f3ac43d947559f315e2e6150f0a
Bach VU <backvu@gmail.com>
2017-05-25 11:55:40 +0200

    Img

--- Java-basic-project/src/JeuGo/Menu.java
+++ Java-basic-project/src/JeuGo/Menu.java
@@ -10,6 +10,8 @@ import java.awt.Label;
 import java.awt.Choice;
 import java.awt.Button;
 import java.awt.TextField;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
 
 public class Menu extends JFrame {
 
@@ -43,6 +45,7 @@ public class Menu extends JFrame {
 		setContentPane(contentPane);
 		
 		JPanel panel = new JPanel();
+		panel.setBorder(null);
 		contentPane.add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
@@ -83,6 +86,67 @@ public class Menu extends JFrame {
 		TextField textField_1 = new TextField();
 		textField_1.setBounds(132, 122, 116, 22);
 		panel.add(textField_1);
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setEnabled(false);
+		btnNewButton.setIcon(new ImageIcon("C:\\Users\\BOB-\\Desktop\\button.png"));
+		btnNewButton.setBounds(69, 199, 22, 21);
+		btnNewButton.setBorderPainted(false);
+		panel.add(btnNewButton);
+		
+		JButton button = new JButton("");
+		button.setEnabled(false);
+		button.setIcon(new ImageIcon("C:\\Users\\BOB-\\Desktop\\button.png"));
+		button.setBounds(69, 180, 22, 21);
+		button.setBorderPainted(false);
+		panel.add(button);
+		
+		JButton button_1 = new JButton("");
+		button_1.setIcon(new ImageIcon("C:\\Users\\BOB-\\Desktop\\button3.png"));
+		button_1.setBorderPainted(false);
+		button_1.setBounds(90, 199, 22, 21);
+		panel.add(button_1);
+		
+		JButton button_2 = new JButton("");
+		button_2.setIcon(new ImageIcon("C:\\Users\\BOB-\\Desktop\\button.png"));
+		button_2.setEnabled(false);
+		button_2.setBorderPainted(false);
+		button_2.setBounds(90, 180, 22, 21);
+		panel.add(button_2);
+		
+		JButton button_3 = new JButton("");
+		button_3.setIcon(new ImageIcon("C:\\Users\\BOB-\\Desktop\\button.png"));
+		button_3.setEnabled(false);
+		button_3.setBorderPainted(false);
+		button_3.setBounds(111, 199, 22, 21);
+		panel.add(button_3);
+		
+		JButton button_4 = new JButton("");
+		button_4.setIcon(new ImageIcon("C:\\Users\\BOB-\\Desktop\\button.png"));
+		button_4.setEnabled(false);
+		button_4.setBorderPainted(false);
+		button_4.setBounds(111, 180, 22, 21);
+		panel.add(button_4);
+		
+		JButton button_5 = new JButton("");
+		button_5.setIcon(new ImageIcon("C:\\Users\\BOB-\\Desktop\\button.png"));
+		button_5.setEnabled(false);
+		button_5.setBorderPainted(false);
+		button_5.setBounds(69, 219, 22, 21);
+		panel.add(button_5);
+		
+		JButton button_6 = new JButton("");
+		button_6.setIcon(new ImageIcon("C:\\Users\\BOB-\\Desktop\\button.png"));
+		button_6.setEnabled(false);
+		button_6.setBorderPainted(false);
+		button_6.setBounds(90, 219, 22, 21);
+		panel.add(button_6);
+		
+		JButton button_7 = new JButton("");
+		button_7.setEnabled(false);
+		button_7.setBorderPainted(false);
+		button_7.setBounds(141, 219, 22, 21);
+		panel.add(button_7);
 		
 		
 	}
